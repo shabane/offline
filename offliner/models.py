81d59201dda8966b4de0782eaeeb5df42d1ab619
shabane <m.mohamadshabane@gmail.com>
2022-07-05 07:12:02 +0430

    feat[list the offline pages, get a page]:
    	1. now the site will list all offline pages by the user logedin.
    	2. ability to download a page now added.

--- offliner/models.py
+++ offliner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Paper(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    path = models.TextField(default='')
--- offliner/views.py
+++ offliner/views.py
@@ -1,23 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import os
 import requests
+from .models import Paper, User
+from bs4 import BeautifulSoup as bs
+from hashlib import blake2b
+import subprocess
 
 def index(request):
-    lst = [
-        {
-            'link': 'https://google.com',
-            'title': 'this is google page'
-        },
-        {
-            'link': 'https://google.com',
-            'title': 'this is another page'
-        }
-    ]
+    lst = []
+    if request.user.is_authenticated:
+        user = request.user
+        if not os.path.exists(f'offpages/{user}'):
+            os.mkdir(f'offpages/{user}')
+        lst = Paper.objects.filter(owner=user)[::-1]
+
     context = {'pages': lst}
     if request.method == 'POST':
         url = request.POST.get('url')
-        if requests.get(url).status_code != 200:
-            return render(request, 'error.html', {'error': 'the url which you provide cant be reached'})
-        os.system(f'cd offpages && wget -E -H -k -p {url}')
+        site = requests.get(url)
+        domain = url.split('/')[2]
+
+        if site.status_code != 200 and site.status_code != 403:
+            return render(request, 'error.html', {'error': f'the url which you provide cant be reached, code: {site.status_code}'})
+
+        page = bs(site.content, 'html.parser')
+        title = page.title.contents[0]
+        dir_name = blake2b(bytes(title, encoding='utf-8')).hexdigest()[::10]
+
+        if not os.path.exists(f'offpages/{user}/{dir_name}'):
+            os.mkdir(f'offpages/{user}/{dir_name}')
+
+        files = list(os.listdir(f'offpages/{user}/{dir_name}'))
+        file_name = 'index.html' if 'index.html' in files else os.path.basename(url)+'.html'
+
+
+        if not os.system(f'cd offpages/{user}/{dir_name} && wget -nd -c -E -H -k -p {url}'):
+            return render(request, 'error.html', {'error': 'site could not download'})
+
+        Paper.objects.create(owner=user, title=title, url=url, path=f'{user}/{dir_name}/{file_name}')
+
     return render(request, 'index.html', context)
