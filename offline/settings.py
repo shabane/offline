6642375c9eb4eb3ebde0122e0182e5a4b7d10c8d
shabane <m.mohamadshabane@gmail.com>
2022-07-12 06:47:28 +0430

    [feat](add logout functionality)

--- offline/settings.py
+++ offline/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 LOGIN_REDIRECT_URL = 'home'
 
+LOGOUT_REDIRECT_URL = 'home'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
--- offline/urls.py
+++ offline/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile', TemplateView.as_view(template_name='profile.html')),
     path('', include('offliner.urls'), name='home'),
 ]
